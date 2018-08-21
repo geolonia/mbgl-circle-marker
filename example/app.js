815d16468bb003306900c595747124967690d64a
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2018-08-21 18:26:32 +0900

    first commit

---
+++ example/app.js
@@ -0,0 +1,67 @@
+import 'babel-polyfill'
+
+import TileCloudControl from '@tilecloud/mbgl-tilecloud-control'
+import ForkMeConntrol from '@tilecloud/mbgl-fork-me-control'
+import ExportControl from '@tilecloud/mbgl-export-control'
+import CircleMarker from '../dist/mbgl-circle-marker'
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'https://tilecloud.github.io/tiny-tileserver/style.json',
+  attributionControl: true,
+  hash: true,
+  localIdeographFontFamily: "sans-serif",
+  interactive: true
+});
+
+map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.GeolocateControl());
+map.addControl(new TileCloudControl());
+map.addControl(new ForkMeConntrol({
+  url: 'https://github.com/tilecloud/mbgl-circle-marker',
+}));
+map.addControl(new ExportControl({
+  dpi: 300,
+}))
+
+new CircleMarker({
+  id: "example-1",
+  minzoom: 7,
+  markers: [
+    {
+      lnglat: [139.741355, 35.658094],
+      label: "東京都",
+      color: "#cccccc",
+      strokeColor: "#cccccc",
+    },
+    {
+      lnglat: [139.642345, 35.447507],
+      label: "神奈川県",
+      color: "#0000ff",
+    },
+    {
+      lnglat: [140.123306, 35.605057],
+      label: "千葉県",
+      color: "transparent",
+      strokeColor: "#cccccc",
+    }
+  ]
+}).addTo(map)
+
+new CircleMarker({
+  id: "example-2",
+  maxzoom: 7,
+  circleStrokeWidth: 100,
+  circleRadius: 10,
+  textSize: 30,
+  markers: [
+    {
+      lnglat: [139.741355, 35.658094],
+      label: "日本",
+      color: "#ff0000",
+      strokeColor: "rgba(255, 0, 0, 0.2)",
+    },
+  ]
+}).addTo(map)
+
+
---
+++ src/index.js
@@ -0,0 +1,85 @@
+'use strict'
+
+class CircleMarker {
+  constructor(options) {
+    this.options = Object.assign({
+        id: "",
+        minzoom: 0,
+        maxzoom: 22,
+        textSize: 14,
+        circleRadius: 6,
+        circleStrokeWidth: 6,
+        markers: [],
+      }, options
+    )
+
+    this.geojson = {
+      type: 'FeatureCollection',
+      features: []
+    };
+
+    for (let i = 0; i < options.markers.length; i++) {
+      const props = Object.assign({
+        color: "#555555",
+        strokeColor: "#555555",
+      }, options.markers[i])
+
+      this.geojson.features.push({
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: options.markers[i].lnglat
+        },
+        properties: props
+      })
+    }
+  }
+
+  addTo(map) {
+    map.on('load', () => {
+      map.addSource(this.options.id, {
+        type: "geojson",
+        data: this.geojson,
+      })
+
+      map.addLayer({
+        "id": `${this.options.id}-marker`,
+        "type": "circle",
+        "minzoom": this.options.minzoom,
+        "maxzoom": this.options.maxzoom,
+        "source": this.options.id,
+        "paint": {
+          "circle-radius": this.options.circleRadius,
+          "circle-color": {"type": "identity", "property": "color"},
+          "circle-stroke-width": this.options.circleStrokeWidth,
+          "circle-stroke-color": {"type": "identity", "property": "strokeColor"}
+        },
+      });
+
+      map.addLayer({
+        "id": `${this.options.id}-label`,
+        "type": "symbol",
+        "minzoom": this.options.minzoom,
+        "maxzoom": this.options.maxzoom,
+        "source": this.options.id,
+        "paint": {
+          "text-color": "#000000",
+          "text-halo-color": "rgba(255, 255, 255, 1)",
+          "text-halo-width": 2,
+        },
+        "layout": {
+          "icon-image": "airport-15",
+          "text-field": "{label}",
+          "text-font": this.options.textFont,
+          "text-size": this.options.textSize,
+          "text-anchor": "top",
+          "text-max-width": 12,
+          "text-offset": [0, 1],
+          "text-allow-overlap": false,
+        }
+      });
+    })
+  }
+}
+
+export default CircleMarker
